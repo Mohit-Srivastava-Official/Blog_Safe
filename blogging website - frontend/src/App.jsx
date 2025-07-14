9188b9fb2df8295fcd3f7a14df69822fc5ef46df
Mohit-Srivastava-Official <mohitsrivastavaofficial@gmail.com>
2025-07-14 22:27:57 +0530

    Updated authentication ui page and routings of sign in and sign up button

--- blogging website - frontend/src/App.jsx
+++ blogging website - frontend/src/App.jsx
@@ -1,20 +1,20 @@
- import { Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/navbar.component";
- 
+import UserAuthForm from "./pages/userAuthForm.page";
+
 const App = () => {
 
   return (
 <Routes>
 <Route path="/" element={<Navbar />}>
-<Route path="signin" element={<h1>Sing in page</h1>} />
-<Route path="signup" element={<h1>Sing up page</h1>} />
+ <Route path="signin" element={<UserAuthForm type="sign-in" />} />
+ <Route path="signup" element={<UserAuthForm type="sign-up" />} />
 </Route>
 </Routes>
 
   );
 
 };
- 
+
 export default App;
 
- 
